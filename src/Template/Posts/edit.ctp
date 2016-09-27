98e6dc107ed86f230aa62295a878a506c2cc7882
kdassigny <dassigny.kevin@hotmail.fr>
2016-09-27 15:47:04 +0200

    module post-it
    ajout du module post-it

---
+++ src/Template/Posts/edit.ctp
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/resources/demos/style.css">
+<style>
+    #draggable {
+        width: 150px;
+        height: 150px;
+        padding: 0.5em;
+        position: fixed;
+        display: block;
+        background-color: #fff893;
+    }
+#hide {
+    display: none;
+}
+</style>
+<button id="button" class="glyphicon glyphicon-eye-open"></button>
+<?= $this->Form->create($post) ?>
+<!--<div id="draggable" class="ui-widget-content" style="padding: 1px;">-->
+<?php
+echo $this->Form->input('description', ['label' => '',
+    'id' => 'draggable',
+    'class' => 'ui-widget-content'
+]); ?>
+<?= $this->Form->button(__('Submit'), ['style' => 'display:none;']); ?>
+<!--</div>-->
+<?= $this->Form->end() ?>
+
+
+<script>
+    $(function () {
+        $("#draggable").draggable();
+    });
+    $(function () {
+        $("#draggable").resizable();
+    });
+
+    $( "#button" ).click(function() {
+        $(this).toggleClass("glyphicon-eye-close");
+        $('.ui-wrapper').toggleClass("hide");
+    });
+
+        $(function () {
+            $("#button").on("click", function () {
+                $("#draggable").animate({
+                    height: 'toggle'
+                });
+            });
+        });
+</script>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+</head>
+<body>
---
+++ tests/Fixture/PostsFixture.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * PostsFixture
+ *
+ */
+class PostsFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'description' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'MyISAM',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'description' => 'Lorem ipsum dolor sit amet',
+            'created' => '2016-09-27 09:32:11',
+            'modified' => '2016-09-27 09:32:11'
+        ],
+    ];
+}
